a349080b71ed3bc69802d127454aede1ab8f86ce
nikola hu <hwqererere@gmail.com>
2019-04-01 17:11:32 +0800

    update by hwq 20190401

---
+++ controllers/DemoController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\controllers;
+use app\models\Demo;
+
+class DemoController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+    	//查
+    	$d=Demo::findBySql('SELECT * FROM Demo')->all();
+    	if($d){
+    		foreach ($d as $key ) {
+    			echo $key->id."\n";
+    			echo $key->keya."\n";
+    		}
+    	}else{
+    		echo "没有数据";
+    	}
+
+    }
+
+    public function actionAdd(){
+    	echo "pppp";
+    	$model = new Demo;
+		$model->keya = 'aaa';
+		$model->keyb  = 'bbb';
+		$model->keyc  = 'ccc';
+		$model->keyd  = 'ddd';
+		if($model->save() > 0){echo "添加成功";echo ",id=".$model->id; }else{echo "添加失败"; } //id自增长，不需要写
+    }
+
+    public function actionEdit(){
+    	$model = Demo::findOne(1);//id
+		$model->keya = '77887';
+		$model->save(); 
+    }
+
+    public function actionDel(){
+    	$count= Demo::model()->deleteAll('username=:name and password=:pass',array(':name'=>'这里填东西',':pass'=>'这里填东西'));
+    	if($count>0){echo"删除成功"; }else{echo "删除失败"; } 
+    }
+
+
+}
